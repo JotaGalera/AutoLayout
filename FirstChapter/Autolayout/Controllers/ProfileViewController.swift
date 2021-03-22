6b547537dd033a8de162cf70d861f7171d7200df
Javier Galera <javigalera94@gmail.com>
2021-03-22 20:21:57 +0100

    Closed #2 finish the first challenge

---
+++ FirstChapter/Autolayout/Controllers/ProfileViewController.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+final class ProfileViewController: UIViewController {
+  // MARK: - IBOutlets
+  @IBOutlet var profileImageView: ProfileImageView!
+  @IBOutlet var fullNameLabel: ProfileNameLabel!
+  
+  // MARK: - Life Cycles
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
